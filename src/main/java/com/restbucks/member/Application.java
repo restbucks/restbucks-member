3957286318210c74c85ce25f65099e5dd6a4d507
hippoom <hippoom@gmail.com>
2016-04-08 15:15:09 +0800

    Support login with Github

--- src/main/java/com/restbucks/member/Application.java
+++ src/main/java/com/restbucks/member/Application.java
@@ -3,10 +3,31 @@ package com.restbucks.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class Application {
+@RestController
+public class Application extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.antMatcher("/**").authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
